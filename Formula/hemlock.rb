3f1d723319a8c64bec1711babb4dce63fa78672f
Ilya Klimenko <Sn0wy-0wl@yandex.ru>
2024-08-18 18:03:53 +0300

    Update hemlock.rb

--- Formula/hemlock.rb
+++ Formula/hemlock.rb
@@ -1,10 +1,10 @@
 class Hemlock < Formula
   desc "Vendor remote files to local folder - CLI"
   homepage "https://github.com/MadL1me/hemlock"
-  url "https://github.com/MadL1me/hemlock/releases/download/v0.0.1/hemlock-mac.tar.gz"
-  sha256 "955c94d237a4127851f3a6bca924f047c821a736ef7ddd845d2698680445d738"
+  url "https://github.com/MadL1me/hemlock/releases/download/v0.0.2/hemlock-mac.tar.gz"
+  sha256 "18da06ae30ae77609494feebea99b0cabd62e2be81b75d267220188848219618"
   license "MIT"
-  version "0.0.1"
+  version "0.0.2"
 
   def install
     bin.install "hemlock"
