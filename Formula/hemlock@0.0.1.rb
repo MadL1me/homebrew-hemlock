fe7ce5a53e454e805881dc8909d3bc1a832a013e
Ilya Klimenko <Sn0wy-0wl@yandex.ru>
2024-08-18 18:02:37 +0300

    Create hemlock@0.0.1.rb

---
+++ Formula/hemlock@0.0.1.rb
@@ -0,0 +1,13 @@
+# hemlock@0.0.1.rb (for version 0.0.1)
+class HemlockAT001 < Formula
+  desc "Vendor remote files to local folder - CLI"
+  homepage "https://github.com/MadL1me/hemlock"
+  url "https://github.com/MadL1me/hemlock/releases/download/v0.0.1/hemlock-mac.tar.gz"
+  sha256 "955c94d237a4127851f3a6bca924f047c821a736ef7ddd845d2698680445d738"
+  license "MIT"
+  version "0.0.1"
+
+  def install
+    bin.install "hemlock"
+  end
+end
